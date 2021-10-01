c75d31e4ab7d80cde364491ce467419cf8917b05
Hella <krisbeaky@gmail.com>
2021-10-01 08:53:57 +0500

    Just deleted all

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,14 +14,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OSEmulator.Views {
+namespace OSEmulator {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            DataContext = new ViewModel.MainWindow();
         }
 
 
